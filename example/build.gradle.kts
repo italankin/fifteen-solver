f04bea961a6fa9064b9251e46d8ef67c56fbdd61
Igor Talankin <italankin@gmail.com>
2024-09-22 14:33:55 +0400

    Fix jitpack build because it can't be so easy

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -2,6 +2,15 @@ plugins {
     alias(libs.plugins.kotlinJvm)
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
 dependencies {
     implementation(project(":solver"))
 }
